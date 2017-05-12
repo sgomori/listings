cc6047c33933e9003ca8df4c9f1435cbe326baba
Steve Gomori <sgomori@vividwind.com>
2017-05-11 22:39:52 -0600

    Added Facebook pixel code.

--- views/inc/header.php
+++ views/inc/header.php
@@ -44,6 +44,22 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <script>
     var base_url = '<?php echo base_url(); ?>';
   </script>
+  
+  <!-- Facebook Pixel Code -->
+  <script>
+  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+  n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+  document,'script','https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '832690430240759'); // Insert your pixel ID here.
+  fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=832690430240759&ev=PageView&noscript=1"
+  /></noscript>
+  <!-- DO NOT MODIFY -->
+  <!-- End Facebook Pixel Code -->
 </head>
 
 <body class="page-fullwidth">
